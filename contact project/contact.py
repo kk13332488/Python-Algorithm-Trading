89fb6decaee6d628f590dab370a4c638d12e2289
kk13332488 <kk13332488@gmail.com>
2021-05-05 01:30:32 +0900

    Contact Project
    Completed version

--- contact project/contact.py
+++ contact project/contact.py
@@ -27,14 +27,55 @@ def print_menu():
     menu = input("메뉴선택: ")
     return int(menu)
 
+def print_contact(contact_list):
+    for contact in contact_list:
+        contact.print_info()
+
+def delete_contact(contact_list, name):
+    for i, contact in enumerate(contact_list):
+        if contact.name == name:
+            del contact_list[i]
+
+def store_contact(contact_list):
+    f = open("contact_db.txt", "wt", encoding = "utf-8")
+    for contact in contact_list:
+        f.write(contact.name + "\n")
+        f.write(contact.phone_number + "\n")
+        f.write(contact.e_mail + "\n")
+        f.write(contact.addr + "\n")
+    f.close()
+
+def load_contact(contact_list):
+    f = open("contact_db.txt", "rt")
+    lines = f.readline()
+    num = len(lines) / 4
+    num = int(num)
+
+    for i in range(num):
+        name = lines[4*i].rstrip('\n')
+        phone = lines[4*i+1].rstrip('\n')
+        email = lines[4*i+2].rstrip('\n')
+        addr = lines[4*i+3].rstrip('\n')
+        contact = Contact(name, phone, email, addr)
+        contact_list.append(contact)
+    f.close()
+
+
 def run():
     contact_list = []
+    load_contact(contact_list)
     while 1:
         menu = print_menu()
         if menu == 1:
             contact = set_contact()
             contact_list.append(contact)
+        elif menu == 2:
+            print_contact(contact_list)
+        elif menu == 3:
+            name = input("Name: ")
+            delete_contact(contact_list, name)
         elif menu == 4:
+            store_contact(contact_list)
             break
 
 if __name__ == "__main__":
